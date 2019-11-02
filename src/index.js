ccd402f3a241996c4dad08e410a78028d7b63951
Max Proske <max@mproske.com>
2019-11-01 17:39:21 -0700

    Add unordered list with three list elements by memory

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <ul>
+        <li>Orange</li>
+        <li>Appple</li>
+        <li>Pear</li>
+    </ul>,
+    document.getElementById('root')
+)

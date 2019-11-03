ed559e961280d5755181f615d36428ab50e2370c
Max Proske <max@mproske.com>
2019-11-03 15:40:01 -0800

    Add functional component with styling by memory

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function MyApp() {
+function MyInfo() {
     return (
+        <>
+        <h1 className="red">Max</h1>
+        <p>I am good at computers 1010100101</p>
         <ul>
-            <li>Orange</li>
-            <li>Appple</li>
-            <li>Pear</li>
+            <li>I</li>
+            <li>D</li>
+            <li>K</li>
         </ul>
+        </>
     )
 }
 
 ReactDOM.render(
-    <MyApp/>,
+    <MyInfo/>,
     document.getElementById('root')
 )

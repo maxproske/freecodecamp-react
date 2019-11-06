a4edcc1fb2f79bb075d92b167e8d3ff6be70885f
Max Proske <max@mproske.com>
2019-11-05 21:50:12 -0800

    Convert functions to classes by memory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+
+// function App() {
+//     return (
+//         <div>
+//             <Header username="Max" />
+//             <Greeting />
+//         </div>
+//     )
+// }
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header username="Max" />
+                <Greeting />
+            </div>
+        )
+    }
+}
+
+// function Header(props) {
+//     return (
+//         <header>
+//             <p>Welcome, {props.username}</p>
+//         </header>
+//     )
+// }
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+            <p>Welcome, {this.props.username}</p>
+        </header>
+        )
+    }
+}
+
+// function Greeting() {
+//     const date = new Date()
+//     const hours = date.getHours()
+//     let timeOfDay = `${hours}:00`;
+
+//     return (
+//         <p>Time of day: {timeOfDay}</p>
+//     )
+// }
+
+class Greeting extends Component {
+    render() {
+        const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay = `${hours}:00`;
+
+        return (
+            <p>Time of day: {timeOfDay}</p>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))

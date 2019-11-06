af2c90d37511095dab3129072ece1c266165d2c9
Max Proske <max@mproske.com>
2019-11-05 21:33:37 -0800

    Finish lesson on class-based components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react'
 import TodoItem from './TodoItem'
 import todosData from './todosData'
 
-function App() {
-    const todoComponents = todosData.map(item => <TodoItem key={item.id} item={item} />)
+// function App() {
+//     const todoComponents = todosData.map(item => <TodoItem key={item.id} item={item} />)
 
-    return (
-        <div className="todo-list">
-            {todoComponents}
-        </div>
-    )
+//     return (
+//         <div className="todo-list">
+//             {todoComponents}
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+    getTodoComponents() {
+        return todosData.map(item => <TodoItem key={item.id} item={item} />)
+    }
+
+    render() {
+        const todoComponents = this.getTodoComponents()
+
+        return (
+            <div className="todo-list">
+                {todoComponents}
+            </div>
+        )
+    }
 }
 
 export default App

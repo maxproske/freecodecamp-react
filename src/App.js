8c7b3a56fd3bb5675177b2fe73e4cf69c8fe40a4
Max Proske <max@mproske.com>
2019-11-18 20:03:17 -0800

    Finish travel form challenge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class App extends Component {
     constructor() {
@@ -7,48 +7,132 @@ class App extends Component {
         this.state = {
             firstName: '',
             lastName: '',
-            isFriendly: false,
-            carType: ''
+            gender: '',
+            destination: '',
+            dietaryRestrictions: {
+                isLactose: false,
+                isVegan: false
+            }
         }
 
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event) {
+        const { name, value, type, checked } = event.target;
 
-        const {name, value} = event.target;
-
-        this.setState({[name]: value})
+        if (type === 'checkbox' && name === 'dietaryRestrictions') {
+            this.setState((prevState) => { 
+                return {
+                    [name]: {
+                        ...prevState.dietaryRestrictions,
+                        [value]: checked
+                    }
+                }
+            })
+        } else {
+            this.setState({
+                [name]: value
+            })
+        }
     }
 
     render() {
+        const nameText = (this.state.firstName || this.state.lastName) &&
+            <h1>Hi, {this.state.firstName} {this.state.lastName}</h1>
+        const genderText = (this.state.gender) && <p>You are {this.state.gender}</p>
+        const destinationText = (this.state.destination) && <p>You want to travel to {this.state.destination}</p>
+
         return (
-            <div>
-                <h1>Hi, {this.state.firstName} {this.state.lastName}</h1>
-                <h2>You drive a {this.state.carType}</h2>
+            <main>
                 <form>
                     <input 
-                        onChange={this.handleChange} 
-                        name="firstName" 
+                        type="text"
                         placeholder="First Name"
+                        name="firstName"
                         value={this.state.firstName}
+                        onChange={this.handleChange}
                     />
+                    
                     <br />
+                
                     <input 
-                        onChange={this.handleChange} 
-                        name="lastName" 
+                        type="text"
                         placeholder="Last Name"
-                        value={this.state.lastName} 
+                        name="lastName"
+                        value={this.state.lastName}
+                        onChange={this.handleChange}
                     />
+                    
                     <br />
-                    <select name="carType" onChange={this.handleChange} >
-                        <option value="volvo">Volvo</option>
-                        <option value="bmw">BMW</option>
+
+                    <label>
+                        <input 
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            checked={this.state.gender === 'male'}
+                            onChange={this.handleChange}
+                        /> Male
+                    </label>
+
+                    <br />
+
+                    <label>
+                        <input 
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            checked={this.state.gender === 'female'}
+                            onChange={this.handleChange}
+                        /> Female
+                    </label>
+
+                    <br />
+
+                    <select
+                        name="destination"
+                        onChange={this.handleChange}
+                    >
+                        <option value="">-- Choose Destination --</option>
+                        <option value="vancouver">Vancouver</option>
+                        <option value="amsterdam">Amsterdam</option>
+                        <option value="tokyo">Tokyo</option>
                     </select>
+
                     <br />
-                    <input type="submit" value="Submit" />
+
+                    <input 
+                        type="checkbox"
+                        name="dietaryRestrictions"
+                        value="isLactose"
+                        onChange={this.handleChange}
+                        defaultChecked={this.state.dietaryRestrictions.isLactose}
+                    /> Lactose intolerant?
+
+                    <br />
+                    
+                    <input 
+                        type="checkbox"
+                        name="dietaryRestrictions"
+                        value="isVegan"
+                        onChange={this.handleChange}
+                        defaultChecked={this.state.dietaryRestrictions.isVegan}
+                    /> Vegan?
+
+                    <br />
+
+                    <button>Submit</button>
                 </form>
-            </div>
+
+                <hr />
+
+                <section>
+                    {nameText}
+                    {genderText}
+                    {destinationText}
+                </section>
+            </main>
         )
     }
 }
